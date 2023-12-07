ed70d9efc0e7b177abb05bc9a06a3415973db144
seocoal <jiyoon369@sookmyung.ac.kr>
2023-12-07 15:05:02 +0900

    11/30

---
+++ smm_object.c
@@ -0,0 +1,32 @@
+#include "smm_common.h"
+#include "smm_object.h"
+#include <string.h>
+
+#define MAX_NODETYPE    7
+#define MAX_GRADE       9
+
+
+
+//object generation
+void smmObj_genNode(void)
+{
+    
+}
+
+
+
+//member retrieving
+
+
+
+//element to string
+char* smmObj_getNodeName(smmNode_e type)
+{
+    return smmNodeName[type];
+}
+
+char* smmObj_getGradeName(smmGrade_e grade)
+{
+    return smmGradeName[grade];
+}
+
